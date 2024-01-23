8037259ea996d06b98b68729962ecad2dab95907
TylerTam <46612488+TylerTam@users.noreply.github.com>
2024-01-23 15:21:15 -0500

    Removed executions
    -Weapons dropped can no longer be picked up
    -Removed reserve count, giving weapons infinite ammo (not infinite clip size)

---
+++ Source/RPG_Template/MyAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "MyAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_TEMPLATE_API UMyAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
